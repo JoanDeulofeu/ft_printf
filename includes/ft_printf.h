b1962d71ef4286c771226a5be7ebe48d0a788f8c
JoanDeulofeu <jgehin@student.42.fr>
2019-05-15 18:22:50 +0200

    Petit nettoyage.

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgehin <jgehin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/15 18:12:32 by jgehin            #+#    #+#             */
+/*   Updated: 2019/05/15 18:12:44 by jgehin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include "../libft/includes/libft.h"
@@ -13,26 +25,6 @@
 /*
 ****	Structure Flags & Convertisseur
 */
-
-typedef struct			s_q_int
-{
-	int					i;
-	int					res;
-	int					tmp;
-	int					bf;
-}						t_q_int;
-
-typedef struct			s_norm
-{
-	int					u;
-	int					lgdb;
-	long long			tmp;
-	long double			save;
-	int					ps;
-	char 				*res;
-	int 				i;
-	long double 		db;
-}						t_norm;
 
 typedef struct			s_flags
 {
@@ -66,6 +58,30 @@ typedef struct			s_conv
 }						t_conv;
 
 /*
+****	Structure de norme
+*/
+
+typedef struct			s_q_int
+{
+	int					i;
+	int					res;
+	int					tmp;
+	int					bf;
+}						t_q_int;
+
+typedef struct			s_norm
+{
+	int					u;
+	int					lgdb;
+	long long			tmp;
+	long double			save;
+	int					ps;
+	char				*res;
+	int					i;
+	long double			db;
+}						t_norm;
+
+/*
 ****	Structure principale
 */
 
@@ -84,15 +100,33 @@ typedef struct			s_s
 }						t_s;
 
 /*
-****	Fonction ft_printf
+****	Fonction d' x to x
 */
 
-int						ft_printf(char *str, ...);
+char					*pf_ftoa(char *res, int i, long double db, t_s *s);
+char					*pf_itoa(char *res, int i, long long nb, int lgnb);
+char					*pf_itoax(char *res, int i, t_s *s, int lgnb);
+char					*pf_itoau(char *res, int i, unsigned long long nb, int
+	lgnb);
+
+/*
+****	Fonction de maths
+*/
+
 int						ft_nbrlen(long long nb);
 int						ft_unbrlen(unsigned long long nb);
 int						ft_ldblen(long double db);
 int						ft_dblen(double db);
 int						ft_plen(t_s *s, unsigned long long unb);
+long long				ft_dec_to_oct(long long nb);
+unsigned int			ft_dec_to_hex(t_s *s, unsigned long long nb);
+long long				ft_absll(long long c);
+char					*ft_hashzero(char *res, char *tmp);
+
+/*
+****	Fonction des convertisseur
+*/
+
 char					*ft_pf_d(t_s *s, long long nb);
 char					*ft_pf_u(t_s *s, unsigned long long nb);
 char					*ft_pf_p(t_s *s, unsigned long long unb);
@@ -100,22 +134,20 @@ char					*ft_pf_x(t_s *s, unsigned long long nb);
 char					*ft_pf_f(t_s *s, long double db);
 char					*ft_pf_s(t_s *s, char *str);
 char					*ft_pf_f2(t_s *s, double db);
+
+/*
+****	Autres fonctions
+*/
+
+int						ft_printf(char *str, ...);
 void					ft_print_param(t_s *s);
 char					*ft_find_conv(t_s *s, int i);
-long long				ft_dec_to_oct(long long nb);
-unsigned int			ft_dec_to_hex(t_s *s, unsigned long long nb);
 char					*ft_xmaj(char *res);
-long long				ft_absll(long long c);
 void					ft_display(t_s *s, char *buff, int bf);
 int						ft_buffering(t_s *s, char *buff, int bf, char *str);
 int						ft_emptybuff(t_s *s, char *buff);
-char					*pf_ftoa(char *res, int i, long double db, t_s *s);
-char					*pf_itoa(char *res, int i, long long nb, int lgnb);
-char					*pf_itoax(char *res, int i, t_s *s, int lgnb);
 int						ft_truelg(t_s *s, int lgdb);
-char					*ft_hashzero(char *res, char *tmp);
 char					*ft_spacetozero(char *res);
-char					*pf_itoau(char *res, int i, unsigned long long nb, int lgnb);
 int						ft_which_flags(t_s *s, int i);
 int						ft_reset_flags(t_s *s);
 int						ft_champ_size(t_s *s, int i);
@@ -136,7 +168,9 @@ char					ft_normsign(t_s *s);
 char					ft_normpart3(t_s *s, int mode);
 char					*ft_normround(t_s *s, char *rs, int i);
 char					*ft_normround(t_s *s, char *rs, int i);
-char					*ft_normpart4x(char *res, int i, t_s *s, unsigned long long nb);
-int						ft_norm_u_more(t_s *s, int lgnb, unsigned long long nb, int u);
+char					*ft_normpart4x(char *res, int i, t_s *s, unsigned
+	long long nb);
+int						ft_norm_u_more(t_s *s, int lgnb, unsigned long
+	long nb, int u);
 
 #endif
